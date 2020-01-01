66e4286d083ff339f0563a8705d0330687c226ae
MrDraven <838413070@qq.com>
2020-01-01 22:41:59 +0800

    配置服务中心

---
+++ trendParentProject/eureka-server/src/main/java/trend/EurekaServerApplication.java
@@ -0,0 +1,20 @@
+package trend;
+
+import cn.hutool.core.util.NetUtil;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        int port = 8761;
+        if(!NetUtil.isUsableLocalPort(port)){
+            System.err.printf("port%disused%n",port);
+            System.exit(1);
+        }
+        new SpringApplicationBuilder(EurekaServerApplication.class).properties("server.port="+port).run(args);
+    }
+}
